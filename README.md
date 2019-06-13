7487d03f8609317148d3ecbcba2de45608be73c9
JULIAN HENAO MARIN <julianhenao@SETIAD.COM.CO>
2019-06-13 14:30:27 -0500

    Add instructions for install and run the tests

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# python-unittest
+# Unit tests in python with unittest
+
+## Requeriments
+
+Only do you need install unittest:
+
+```console
+pip install unittest
+```
+
+## How run the tests:
+
+- Run in the command line:
+
+```console
+Python tests.py
+```
+
+### Or
+
+- Run in the command line:
+
+```console
+python -m unittest discover -s tests
+```
--- tests.py
+++ tests.py
@@ -2,11 +2,18 @@ import unittest
 import example
 
 class WorkingExmapleTests(unittest.TestCase):
-    def test_area(self):
-        self.assertIsInstance (example.area(5, 5), int)
 
+    # area(a, b)
+    def test_area(self):
+        self.assertEqual(example.area(5, 5), 25, "Should be 25")
+
+    # area(a, b)
+    def test_area_two(self):
+        assert example.area(2, 3) == 6
+
+    # full_name(a, b)
     def test_full_name(self):
-        self.assertIsInstance (example.full_name("Keanu", "Reeves"), str)
+        self.assertIsInstance(example.full_name("Keanu", "Reeves"), str)
 
 if __name__ == '__main__':
     unittest.main()
